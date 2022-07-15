63fe5daaf9b5cbcc6be1fd52cbe157615b0e9a60
Nikolas Rialas <NRialas@dits.dmrid.gov.cy>
2022-07-15 11:57:03 +0300

    review construction

--- src/dsf-service-template-net6/Pages/ReviewPage.cshtml.cs
+++ src/dsf-service-template-net6/Pages/ReviewPage.cshtml.cs
@@ -1,10 +1,14 @@
+using dsf_service_template_net6.Data.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace dsf_service_template_net6.Pages
 {
+    [BindProperties]
     public class ReviewPageModel : PageModel
     {
+        public CitizenDataResponse _application = new CitizenDataResponse();
+
         public void OnGet()
         {
         }
